98ac1c24a169cba3f766e9041689a89c192009c6
rmohan17 <65498856+rmohan17@users.noreply.github.com>
2023-05-13 05:43:15 -0400

    changes for v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!!!</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
